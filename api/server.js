5f67e3820b9d5f1efb96582aac8be6513bd79ac6
Eric Ferguson <turtle0888@gmail.com>
2020-02-25 16:26:34 -0600

    Add cookie and session to server

--- api/server.js
+++ api/server.js
@@ -1,12 +1,35 @@
 const express = require('express');
 const helmet = require('helmet');
 const cors = require('cors');
+const session = require('express-session');
+const KnexStore = require('connect-session-knex')(session);
 
 const authRouter = require('../auth/auth-router.js');
 const usersRouter = require('../users/users-router.js');
 const restricted = require('../auth/restricted-middleware.js');
+const knex = require('../database/dbConfig.js');
 
 const server = express();
+
+const sessionConfig = {
+  name: 'cookie monster',
+  secret: 'keep it secret, keep it safe',
+  resave: false,
+  saveUninitialized: true,
+  cookie: {
+    maxAge: 1000 * 60 * 10,
+    secure: false,
+    httpOnly: true
+  },
+
+  store: new KnexStore({
+    knex,
+    tablename: 'sessions',
+    createtable: true,
+    sidfieldname: 'sid',
+    clearInterval: 1000 * 60 * 15
+  })
+};
 
 server.use(helmet());
 server.use(express.json());
